3a2e560dd06cc48bcc3efd6fa672548cea7c3f70
Mahendra <mahendrabrowny@gmail.com>
2022-08-17 23:04:57 +0530

    Explore page ui

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// Images
+import rentImage from "../../assets/jpg/rentCategoryImage.jpg";
+import sellImage from "../../assets/jpg/sellCategoryImage.jpg";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentImage}
+              alt="houseImage"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for Rent</p>
+          </Link>
+
+          <Link to="/category/sell">
+            <img
+              src={sellImage}
+              alt="houseImage"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for Sell</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Explore;
