1933c2b123f6d4d977ee0f83c1c6674f73c82824
Mahendra <mahendrabrowny@gmail.com>
2022-08-17 17:22:37 +0530

    profile component with edit details functionality

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -49,51 +49,51 @@ const Profile = () => {
   };
 
   return (
-    // <div className="profile">
-    //   <header className="profileHeader">
-    //     <p className="pageHeader">My Profile</p>
-    //     <button type="button" onClick={handleLogOut} className="logOut">
-    //       Log Out
-    //     </button>
-    //   </header>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" onClick={handleLogOut} className="logOut">
+          Log Out
+        </button>
+      </header>
 
-    //   <main>
-    //     <div className="profileDetailsHeader">
-    //       <p className="profileDetailsText">Personal Details</p>
-    //       <p
-    //         className="changePersonalDetails"
-    //         onClick={() => {
-    //           changeDetails && onSubmit();
-    //           setChangeDetails((prevState) => !prevState);
-    //         }}
-    //       >
-    //         {changeDetails ? "Done ✔" : "Change"}
-    //       </p>
-    //     </div>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "Done ✔" : "Change"}
+          </p>
+        </div>
 
-    //     <div className="profileCard">
-    //       <form>
-    //         <input
-    //           type="text"
-    //           id="name"
-    //           className={!changeDetails ? "profileName" : "profileNameActive"}
-    //           disabled={!changeDetails}
-    //           value={name}
-    //           onChange={handleChange}
-    //         />
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={handleChange}
+            />
 
-    //         <input
-    //           type="text"
-    //           id="email"
-    //           className={!changeDetails ? "profileEmail" : "profileEmailActive"}
-    //           disabled={!changeDetails}
-    //           value={email}
-    //           onChange={handleChange}
-    //         />
-    //       </form>
-    //     </div>
-    //   </main>
-    // </div>
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </main>
+    </div>
   );
 };
 
