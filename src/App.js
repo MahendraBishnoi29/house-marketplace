36c365f73cac817cf83b46edaecfd10dd86a3d82
Mahendra <mahendrabrowny@gmail.com>
2022-08-17 21:42:57 +0530

    unused imports removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
 import ForgotPassword from "./components/Auth/ForgotPassword";
 import SignIn from "./components/Auth/SignIn";
 import SignUp from "./components/Auth/SignUp";
