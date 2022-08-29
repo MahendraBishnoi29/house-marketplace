fb9755555d0802ad54865900a1e09f6fe0a453be
Mahendra <mahendrabrowny@gmail.com>
2022-08-29 15:52:31 +0530

    structure of create listing done

--- src/pages/createListing/CreateListing.jsx
+++ src/pages/createListing/CreateListing.jsx
@@ -1,6 +1,52 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../../components/Spinner/Spinner";
 
 const CreateListing = () => {
+  const [formData, setFormData] = useState({
+    type: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  });
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid });
+        } else {
+          navigate("/sign-in");
+        }
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return <div>CreateListing</div>;
 };
 
