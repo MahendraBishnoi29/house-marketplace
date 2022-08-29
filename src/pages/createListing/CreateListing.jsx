65887cb01be79222c60558f3b25e4dc0cf3226d2
Mahendra <mahendrabrowny@gmail.com>
2022-08-29 18:43:27 +0530

    upload image to firestore functionality done

--- src/pages/createListing/CreateListing.jsx
+++ src/pages/createListing/CreateListing.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable default-case */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Spinner from "../../components/Spinner/Spinner";
+import { v4 as uuidv4 } from "uuid";
 
 const CreateListing = () => {
   const [formData, setFormData] = useState({
@@ -104,6 +112,60 @@ const CreateListing = () => {
       geoLocation.lng = longitude;
       location = address;
     }
+
+    // Storing Images in Firestore
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, "images/" + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            switch (error.code) {
+              case "storage/unauthorized":
+                break;
+              case "storage/canceled":
+                break;
+              case "storage/unknown":
+                break;
+            }
+          },
+          () => {
+            // Upload completed successfully, now we can get the download URL
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((err) => {
+      setLoading(false);
+      toast.error("Failed to Upload Images!");
+    });
+
     setLoading(false);
   };
 
