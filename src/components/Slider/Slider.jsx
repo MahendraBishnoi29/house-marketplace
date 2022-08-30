f3ae2f01cdc8981ee2d9a5f433c6a63ce90c0eb9
Mahendra <mahendrabrowny@gmail.com>
2022-08-30 17:54:52 +0530

    slider component

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const Slider = () => {
+  return <div>Slider</div>;
+};
+
+export default Slider;
